a54240ef877b17965dd1979685028c2a68b445bd
weihan <zhengweihan.91@gmail.com>
2017-10-04 14:34:37 +0000

    auto-commit

---
+++ chatscripts/gscript2.py
@@ -0,0 +1,25 @@
+
+import httplib2
+import argparse
+import requests
+import json
+api_key = "AIzaSyAWd44FwD80rlxc8S6CZYUdjRQw_9cjeWg"
+def gm_search(params):
+    params = params.strip().replace(" ", "+")
+    query = params
+    req = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+query+"&key="+api_key
+    print("request URL: %s"%(req))
+    resp = requests.get(req)
+    content = json.loads(resp.text)
+    return content
+if __name__=="__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('search',help = '', type=str, nargs='+')
+    args = argparser.parse_args()
+    search = " ".join(args.search)
+    print("Fetching data for %s"%(search))
+    data = gm_search(search)
+    print("Writing data to output fe")
+    with open('gscript_results.json','w') as fp:
+        json.dump(data, fp)
+        
