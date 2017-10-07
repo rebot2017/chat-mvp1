c23ed567c7ccbc14b71cc91b122a298c1ff8bcb6
zhengwe2 <zhengweihan@gic.com.sg>
2017-10-07 16:49:26 +0800

    add more scripts

---
+++ chatapp/team3/wikiscript.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import argparse
+
+def wiki_search(query):
+    query = query.strip().replace(" ", "%20")
+    search_request_url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srwhat=nearmatch&srsearch=%s&format=json&utf8="%(query)
+
+    search_result = requests.get(search_request_url)
+    json_result = json.loads(search_result.text)
+    result_list = json_result['query']['search']
+    if len(result_list) > 0:
+        title = result_list[0]['title']
+        article_request_url = "https://en.wikipedia.org/wiki/%s"%(title.replace(" ", "_"))
+        result = requests.get(article_request_url)
+        soup = BeautifulSoup(result.text, 'html.parser')
+        first_paragraph = soup.find(attrs={'class': 'mw-parser-output'}).p.get_text()
+        result_object = {'title': title, 'description': first_paragraph}
+        print(json.dumps(result_object))
+
+if __name__=="__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('title',help = '', type=str, nargs='+')
+    args = argparser.parse_args()
+    title = " ".join(args.title)
+    wiki_search(title)
