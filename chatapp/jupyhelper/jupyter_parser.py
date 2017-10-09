056d2822d3b340226d2551b42938892a0465ee8e
zhengwe2 <zhengweihan@gic.com.sg>
2017-10-09 17:06:02 +0800

    add scripts

--- chatapp/jupyhelper/jupyter_parser.py
+++ chatapp/jupyhelper/jupyter_parser.py
@@ -7,6 +7,11 @@ class jupyter_helpers:
     def __init__(self):
         print("init helpers")
 
+    def jupyter_run(self, infile, outfile):
+        self.jupyter_parse_notebook(infile, outfile)
+        self.jupyter_copy_script(outfile)
+        self.jupyter_commit()
+    
     def jupyter_parse_notebook(self, infile, outfile):
         print("Parsing File: %s"%infile)
         with open(infile, "r") as f:
@@ -21,22 +26,42 @@ class jupyter_helpers:
                             continue
                         else:
                             evalStr+=line
+                            if not line.endswith('\n'):
+                                evalStr+='\n'
             wfile =  open(outfile, 'w')
             wfile.write(evalStr)
             wfile.close()
             print("file parsed and written to %s."%outfile)
-            curdir = os.getcwd()
-            filetocopy = os.path.abspath(os.path.join(curdir, outfile))
-            parentdir = os.path.abspath(os.path.join(curdir,os.pardir))
-            parentdir = os.path.abspath(os.path.join(parentdir, os.pardir))
-            scriptDir = os.path.abspath(os.path.join(parentdir, "chatscripts"))
-            print("Depositing file in %s"%scriptDir)
-            shutil.copy(filetocopy, scriptDir)
-            time.sleep(1)
-            print("Flushed file, committing")
-            os.system("sudo bash /home/user/chat-mvp1/git.sh")
-            print("committed to github")
             
-
-
     
+    def jupyter_copy_script(self, outfile):
+        curdir = os.getcwd()
+        filetocopy = os.path.abspath(os.path.join(curdir, outfile))
+        parentdir = os.path.abspath(os.path.join(curdir,os.pardir))
+        parentdir = os.path.abspath(os.path.join(parentdir, os.pardir))
+        scriptDir = os.path.abspath(os.path.join(parentdir, "chatscripts"))
+        print("Depositing file in %s"%scriptDir)
+        shutil.copy(filetocopy, scriptDir)
+        print("Copied File")
+    
+    def jupyter_commit(self):
+        time.sleep(1)
+        print("committing...")
+        os.system("bash /home/user/chat-mvp1/git.sh")
+        print("committed to github")
+    
+    def jupyter_deploy(self, infile, outfile, argument=""):
+        TEST_FILE_NAME = "jupyter_test_file.py"
+        self.jupyter_parse_notebook(infile, TEST_FILE_NAME)
+        print("Written file to %s. Evaluating..."%(TEST_FILE_NAME))
+        with open(TEST_FILE_NAME, 'r') as f:
+            file_content = f.readlines()
+            file_str = ""
+            for line in file_content:
+                file_str += line
+            try:    
+                eval(file_str)
+                jupyter_run(infile, outfile)
+            except SyntaxError as e:
+                print("Syntax Errors detected. Please correct them before committing.")
+            
--- chatscripts/gscript.py
+++ chatscripts/gscript.py
@@ -1,26 +1,29 @@
-
-import httplib2
 import argparse
+import requests
 import json
-api_key = "AIzaSyAWd44FwD80rlxc8S6CZYUdjRQw_9cjeWg"
-
-
+API_KEY = "AIzaSyAWd44FwD80rlxc8S6CZYUdjRQw_9cjeWg"
+MAPS_BASIC_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&key=%s"
+MAPS_PHOTO_URL = "https://maps.googleapis.com/maps/api/place/photo?photoreference=%s&maxheight=400&key=%s"
 def gm_search(params):
-    params = params.strip().replace(" ", "+")
-    query = params
-    req = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+query+"&key="+api_key
-    print("request URL: %s"%(req))
-    resp, content = httplib2.Http().request(req)
-    content = json.loads(content.decode('utf-8'))
-    return content
+    query = params.strip().replace(" ", "+")
+    request_string = MAPS_BASIC_URL%(query,API_KEY)
+    resp = requests.get(request_string)
+    places = json.loads(resp.text)['results']
+    
+    result_objs = []
+    for place in places:
+        place_name = place['name']
+        place_address = place['formatted_address']
+        photo_request_url = MAPS_PHOTO_URL%(place['photos'][0]['photo_reference'], API_KEY)
+        desc = "Address: " + place_address
+        result_objs.append({'img': photo_request_url, 'title': place_name, 'description': desc})
+    print(json.dumps(result_objs, indent=4))
 if __name__=="__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument('search',help = '', type=str, nargs='+')
     args = argparser.parse_args()
     search = " ".join(args.search)
-    print("Fetching data for %s"%(search))
-    scraped_data = gm_search(search)
-    print("Writing data to output fe")
-    with open('gscript_results.json','w') as fp:
-        json.dump(scraped_data, fp)
-        
+    
+    gm_search(search)
+        import sys
+sys.path.append("/Users/weihan/randomgits/chat-mvp1/chatapp")
