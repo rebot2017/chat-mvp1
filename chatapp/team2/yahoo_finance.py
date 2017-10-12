6e2cbfba2e595d1265a903f1b66cbbaab387d2c2
weihan <zhengweihan.91@gmail.com>
2017-10-12 05:46:14 +0000

    update notebooks

--- chatapp/team2/yahoo_finance.py
+++ chatapp/team2/yahoo_finance.py
@@ -1,15 +1,13 @@
 from lxml import html  
 import requests
-from exceptions import ValueError
 from time import sleep
 import json
 import argparse
 from collections import OrderedDict
 from time import sleep
-def parse(ticker):
+def get_ticker_data(ticker):
     url = "http://finance.yahoo.com/quote/%s?p=%s"%(ticker,ticker)
     response = requests.get(url)
-    print "Parsing %s"%(url)
     sleep(4)
     parser = html.fromstring(response.text)
     summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr')
@@ -31,15 +29,10 @@ def parse(ticker):
         table_value = ''.join(raw_table_value).strip()
         summary_data.update({table_key:table_value})
     summary_data.update({'1y Target Est':y_Target_Est,'EPS (TTM)':eps,'Earnings Date':earnings_date,'ticker':ticker,'url':url})
-    print(json.dumps(summary_data))
-
+    return summary_data
 if __name__=="__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument('ticker',help = '')
     args = argparser.parse_args()
     ticker = args.ticker
-    print "Fetching data for %s"%(ticker)
-    scraped_data = parse(ticker)
-    print "Writing data to output file"
-    with open('%s-summary.json'%(ticker),'w') as fp:
-         json.dump(scraped_data,fp,indent = 4)
+    print(json.dumps(get_ticker_data(ticker))
