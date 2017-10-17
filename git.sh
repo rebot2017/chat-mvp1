064f082bcdc2f90530f5bfa4566d4fdfd87eb807
weihan <zhengweihan.91@gmail.com>
2017-10-17 06:52:45 +0000

    fix error in commit script

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 git -C /home/user/chat-mvp1/ fetch --all
-git -C /home/user/chat-mvp1/ teset --hard origin/master
+git -C /home/user/chat-mvp1/ reset --hard origin/master
 git -C /home/user/chat-mvp1/ add /home/user/chat-mvp1/chatscripts/*.py
 git -C /home/user/chat-mvp1/ commit -m "auto-commit"
 git -C /home/user/chat-mvp1/ push
