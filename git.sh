2d2e8a29c948fbf3d9426b31fa12ed4b1f89bb7a
weihan <zhengweihan.91@gmail.com>
2017-10-19 07:49:58 +0000

    team-based git commits

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 git -C /home/user/chat-mvp1/ fetch --all
 git -C /home/user/chat-mvp1/ reset --hard origin/master
-git -C /home/user/chat-mvp1/ add /home/user/chat-mvp1/chatscripts/*.py
-git -C /home/user/chat-mvp1/ commit -m "auto-commit"
+git -C /home/user/chat-mvp1/ add /home/user/chat-mvp1/chatscripts/$1_*.py /home/user/chat-mvp1/chatapp/$1/*.ipynb /home/user/chat-mvp1/$1/*.py
+git -C /home/user/chat-mvp1/ commit -m "$1 auto-commit"
 git -C /home/user/chat-mvp1/ push
