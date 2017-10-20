fab304a41252bc425872930280806fd57d90f7e2
weihan <zhengweihan.91@gmail.com>
2017-10-20 00:13:38 +0000

    save notebook when committing

--- custom/custom.js
+++ custom/custom.js
@@ -15,10 +15,10 @@ if($(IPython.toolbar.selector.concat(' > #kill-run-all')).length == 0){
 
 var commit_cell = function(code){
        var nb = Jupyter.notebook
-
+    nb.save_notebook(false).then(function(){;
     // create cell at top
-    nb.select(0)
-    nb.insert_cell_above()
+    nb.select(0);
+    nb.insert_cell_above();
 
     // select cell below (the one we have created)
     var cell = nb.select(0).get_selected_cell()
@@ -34,8 +34,8 @@ var commit_cell = function(code){
 console.log(cell);
 console.log("executed, deleting cell")
 $(cell.events).one('finished_execute.CodeCell', function(){setTimeout(function(){Jupyter.notebook.delete_cell(0);},3000);});
+});
 }
-
 if($(IPython.toolbar.selector.concat(' > #commit-to-git')).length == 0){
   IPython.toolbar.add_buttons_group([
         {
