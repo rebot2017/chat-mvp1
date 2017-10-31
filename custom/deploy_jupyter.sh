3c47a56e1eb48bc16de65b6764de0b62474fe481
weihan <keep.it.that.way@gmail.com>
2017-10-31 12:12:35 +0000

    fix deploy script

--- custom/deploy_jupyter.sh
+++ custom/deploy_jupyter.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+#update git
+  git config --global user.email "keep.it.that.way@gmail.com"
+  git config --global user.name "weihan"
 
+apt update
 #installing dependencies
-apt install python3
-apt install pip3
+apt install -y python3
+apt install -y python3-pip
+apt install -y whois
 pip3 install jupyterhub
-apt install npm nodejs-legacy
+apt-get install npm nodejs-legacy
 npm install -g configurable-http-proxy
-pip3 install --upgrade notebook
+pip3 install notebook
 
 
 # adding custom authentication hook
 cp auth.py /usr/local/lib/python3.5/dist-packages/jupyterhub/
 
 #copy refs to /home
-cp -R refs/ /home/refs/
-
-
+cp -R refs/ /home/
+mkdir /home/chatscripts
+chmod 777 -R /home/chatscripts
