bf899e715e7d88d63d5862c1e0e4ea78228ff9e0
weihan <keep.it.that.way@gmail.com>
2017-11-02 01:31:15 +0000

    change user permissions

--- custom/add_user.sh
+++ custom/add_user.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 useradd -p `mkpasswd 123` $1
-mkdir /home/$1
+dir=/home/$1
 
-cp -R /home/refs/* /home/$1
+if [ ! -d "$dir" ]; then
+	mkdir /home/$1
 
-mkdir /home/$1/.jupyter
-mkdir /home/$1/.jupyter/custom
-cp /root/chat-mvp1/custom/custom.js /home/$1/.jupyter/custom/
-chown -R $1 /home/$1
+	cp -R /home/refs/* /home/$1
+
+	mkdir /home/$1/.jupyter
+	mkdir /home/$1/.jupyter/custom
+	cp /root/chat-mvp1/custom/custom.js /home/$1/.jupyter/custom/
+	chown -R $1 /home/$1
+	chmod -R 777 /home/$1
+fi
